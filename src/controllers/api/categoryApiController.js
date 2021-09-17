24115355bc8bbaba7b61c337b02160cc6830331a
hrcostagarcia <hrcostagarcia@gmail.com>
2021-09-17 02:09:10 -0300

    se agrega api categoria

--- src/controllers/api/categoryApiController.js
+++ src/controllers/api/categoryApiController.js
@@ -1,9 +1,25 @@
 const { Category } = require('../../database/models')
+const { Product } = require('../../database/models')
 
 module.exports = {
     async listCategory(req, res) {
         try {
             const category = await Category.findAndCountAll({
+            })
+            const products = await Product.findAndCountAll({
+                attributes: ["category_id"]
+                
+            })
+            console.log(products)
+            
+
+            
+            const prueba = Product.count(
+                { where: {'category_id': 1}
+            })
+            var categoryList = category.rows.map(function(category){
+                category.setDataValue("Total", {prueba}
+                )
             })
 
             res.status(200).json({
